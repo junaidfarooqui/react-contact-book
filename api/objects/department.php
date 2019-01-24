b65007ee88eabd8b65c61880829401f4b8636738
junaidfarooqui <junaidfarooqui61@gmail.com>
2019-01-24 22:39:36 +0100

    department class ready

---
+++ api/objects/department.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @var $this Department Class
+ *
+ * @author Junaid Farooqui
+ */
+class Department{
+
+    // database connection and table name
+    private $conn;
+    private $table_name = "department";
+
+    // object properties
+    public $id;
+    public $name;
+    public $description;
+    public $created;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    // used by select drop-down list
+    public function readAll(){
+        //select all data
+        $query = "SELECT
+                    id, name, description
+                FROM
+                    " . $this->table_name . "
+                ORDER BY
+                    name";
+
+        $stmt = $this->conn->prepare( $query );
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
